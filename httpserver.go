2982859a822878ced5e2ff63c6c183b0238f23d6
ahnselina <ahnselina@gmail.com>
2022-01-10 01:46:31 +0800

    Add files via upload

---
+++ httpserver.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/healthz", healthz)
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "200\n")
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	ip, err := GetIP(r)
+	if err != nil {
+		fmt.Errorf("Get request client ip failed:%v.\n", err)
+	}
+	fmt.Printf("%s entering root handler\n", ip)
+	w.Header().Set("VERSION", os.Getenv("VERSION"))
+	w.WriteHeader(500)
+	user := r.URL.Query().Get("user")
+	if user != "" {
+		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
+	} else {
+		io.WriteString(w, "hello [stranger]\n")
+	}
+	io.WriteString(w, "===================Details of the http request header:============\n")
+	for k, v := range r.Header {
+		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+	}
+}
+
+// GetIP returns request real ip.
+func GetIP(r *http.Request) (string, error) {
+	ip := r.Header.Get("X-Real-IP")
+	if net.ParseIP(ip) != nil {
+		return ip, nil
+	}
+
+	ip = r.Header.Get("X-Forward-For")
+	for _, i := range strings.Split(ip, ",") {
+		if net.ParseIP(i) != nil {
+			return i, nil
+		}
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+
+	if ip == "::1" {
+		return "127.0.0.1", nil
+	}
+	if net.ParseIP(ip) != nil {
+		return ip, nil
+	}
+
+	return "", errors.New("no valid ip found")
+}
